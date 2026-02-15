1b686b8ab49853dc1f45b2eda5e43ae9d2eaeefc
datomiqava971-dot <datomiqava971@gmail.com>
2026-02-15 14:06:28 +0400

    second commit

---
+++ level2/claswork.py
@@ -0,0 +1,12 @@
+
+print(1+1)
+print(5-3)
+print(2*5)
+print(8/2)
+
+
+name="dvit"
+age=16
+color="blue"
+
+print("my name is " + name + ", i am 16 years old " + "my favourite color is " + color)
---
+++ level2/homework.py
@@ -0,0 +1,47 @@
+#string არის ყველაფერი რაც იწერება ბრჭყალებში
+#integer არის ნებისიერი მთელი რიცხვითი მნიშვნელობა
+#float არის ნებისმიერი ატწილადი მნიშვნელობა 
+#variables-ცვლადები. ცვლადებს ვიყენებთ იმისთვის რომ შევნახოთ შესაბამისი მნიშვნელობა
+
+print("ხე")
+print("ცა")
+print("მიწა")
+print("ღრუბელი")
+print("მზე")
+print(5)
+print(3)
+print(8)
+print(15)
+print(33)
+print(3.4)
+print(7.1)
+print(11.8)
+print(86.7)
+print(55.5)
+
+country="south africa"
+city=" Pretoria"
+person="elon musk"
+goal=" to secure the long-term survival"
+status="real chad"
+
+print(person + " born in " + country + ", in" + city + ". elon's goal is" + goal + ". he is " + status)
+
+print(4+7)
+print(9+3)
+print(1.3+55)
+print(54-11)
+print(100-3.4)
+print(6.5-80)
+print(7*8)
+print(3.3*42)
+print(14*9)
+print(4/2.2)
+print(1/0.5)
+print(100/30)
+
+num1="18"
+num2="4"
+num3="71"
+
+print(num1 + num2 + num3)
