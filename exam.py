7fd811449a481f6324a8f50a7acfe08acb8a5835
datomiqava971-dot <datomiqava971@gmail.com>
2026-02-28 15:45:56 +0400

    second commit

---
+++ exam.py
@@ -0,0 +1,6 @@
+print(5 + 6)
+print(10 -4)
+print(4 * 7)
+print(12 / 6)
+print(4 // 3)
+print(5 % 3)
---
+++ level3/homework.py
@@ -0,0 +1,40 @@
+
+#კომენტარს ვიყენებთ იმისთვის რომ ადამიანებს გავუმარტივოთ კოდის წაკკითხვის და აღქმის პროცესი, ამისთვის ვიყენებთ # ნიშანს
+
+#case-sensitive არი პროგრამირების ენა რომლისთვისაც დიდი მნიშვნელობა აქვს იმას თუ როგორ რეგისტრში იქნება კოდი დაწერილი.
+
+#case-insensetive არი პროგრამირების ენა რომლისთვისაც დიდი მნიშვნელობა არ აქვს იმას თუ როგორ რეგისტრში იქნება კოდი დაწერილი.
+
+
+#snake_case-ი არის ჩაწერის სახე რომლითაც ვაერთებთ ცვლადის დასახელებაში არსებულ ორ სიტყვას 
+
+movie_title = "The Matrix"
+snack_name = "Doritos"
+
+print("we are watching " + movie_title + " with " + snack_name )
+
+
+#bug ნიშნავს შეცდომას ხოლო debugging ამ შეცდომის შესწორებას
+
+
+name = "Lika"
+print(name)
+
+number = 5
+text = " apples"
+print(number * text)
+
+name = "liKa"
+name2 = name + "4"
+
+first_user = "Lika"
+nduser2 = "Giorgi"
+print(first_user)
+
+first_name = "Data"
+last_name = "random"
+
+first_user = "Lika"
+nduser2 = "Giorgi"
+print(first_user)
+
