90e07b295dc211fee772dfd88e232c70e4af9a89
datomiqava971-dot <datomiqava971@gmail.com>
2026-04-04 13:41:08 +0400

    second commit

---
+++ exams/exam1.py
@@ -0,0 +1,12 @@
+print("tbilisi")
+print(55)
+print(32.9)
+
+car_brand = 'bmw'
+print(car_brand)
+
+name=input('შემოიყვანეთ თქვენი სახელი: ')
+print(name)
+
+age=int(input('შემოიყვანეთ თქვენი ასაკი: '))
+print(age + 10)
---
+++ level8/claswork.py
@@ -0,0 +1,24 @@
+# logical operators არის ლოგიკური მოქმედებები რომელშიც შედის and და or კავშირები, მათი გამოენებით შეგვიძლია გათ მოჩემული პირობა შესრულდა თუ არა.
+
+
+print(100<543)
+print(43<69)
+print(341<500)
+
+print(32>4)
+print(66>8)
+print(32>30)
+
+print(45>=45)
+print(26>=20)
+print(18>=33)
+
+print(53<=53)
+print(72<=90)
+print(21<=45)
+
+
+print(True and False)
+print(True and True)
+print(False or True)
+print(False or False)
