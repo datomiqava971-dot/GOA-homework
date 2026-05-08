5e3adc3ccb612200f48317fd7f43e1dde64a592c
datomiqava971-dot <datomiqava971@gmail.com>
2026-05-09 00:53:29 +0400

    second commit

---
+++ level12/claswork.py
@@ -0,0 +1,16 @@
+for i in range(1, 100):
+    print(i)
+
+for i in range(100, 201):
+    print(i)
+
+
+index1=10
+while index1 > 1:
+    print(index1)
+    index1 -= 1
+
+index2=100
+while index2 > 0:
+    print(index2)
+    index2 -= 2
